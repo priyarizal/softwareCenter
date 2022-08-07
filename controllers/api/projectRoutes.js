f3f3487cc6161c85479823060bbaee0cfe445eb6
priyarizal <103598087+priyarizal@users.noreply.github.com>
2022-08-07 17:50:15 -0400

    created projected routes- work in progress

---
+++ controllers/api/projectRoutes.js
@@ -0,0 +1,16 @@
+const router = require('express').Router();
+const { Project } = require('../../models');
+const withAuth = require('../../utils/auth');
+
+router.post('/', withAuth, async (req, res) => {
+    try {
+      const newProject = await Project.create({
+        ...req.body,
+        user_id: req.session.user_id,
+      });
+  
+      res.status(200).json(newProject);
+    } catch (err) {
+      res.status(400).json(err);
+    }
+  });
--- controllers/index.js
+++ controllers/index.js
@@ -1,9 +1,13 @@
- 
+
 const router = require('express').Router();
 const apiRoutes = require("./api")
-const htmlRoutes = require ("./htmlRoutes")
+const htmlRoutes = require("./htmlRoutes")
+const projectRoutes = require("./api/projectRoutes")
 
 router.use(htmlRoutes)
+
+
+// router.use(projectRoutes)
 
 router.use("/api", apiRoutes)
 
