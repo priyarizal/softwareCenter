0654e008e57c8623bd8eb2f1f14b00d61de7a41c
priyarizal <103598087+priyarizal@users.noreply.github.com>
2022-08-08 00:56:18 -0400

    requiring projectRoutes

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,8 +1,10 @@
 const router = require('express').Router();
 
 const userRoutes = require('./userRoutes.js')
+const projectRoutes = require("./projectRoutes")
 
 router.use('/user', userRoutes)
+router.use('/project', projectRoutes)
 
 // router.use('/images')
 
