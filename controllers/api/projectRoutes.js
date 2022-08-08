24f268464e8125dcf2c54223679af2859d8673b6
priyarizal <103598087+priyarizal@users.noreply.github.com>
2022-08-08 00:38:37 -0400

    minor changes on routes for new project

--- controllers/api/projectRoutes.js
+++ controllers/api/projectRoutes.js
@@ -1,16 +1,20 @@
 const router = require('express').Router();
-const { Project } = require('../../models');
+const { Project } = require('../models');
 const withAuth = require('../../utils/auth');
 
 router.post('/', withAuth, async (req, res) => {
-    try {
-      const newProject = await Project.create({
-        ...req.body,
-        user_id: req.session.user_id,
-      });
-  
-      res.status(200).json(newProject);
-    } catch (err) {
-      res.status(400).json(err);
-    }
-  });
+  try {
+    const newProject = await Project.create({
+      ...req.body,
+      user_id: req.session.user_id,
+    });
+
+    res.status(200).json(newProject);
+  } catch (err) {
+    res.status(400).json(err);
+  }
+});
+
+
+module.exports = router;
+
